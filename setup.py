ef1106038ec0640c3023669be403291021e78d0f
Li Xing <lix930701@gmail.com>
2020-01-14 05:59:14 +0000

    v0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dnachisel_dtailor_mode", # Replace with your own username
-    version="0.0.2",
+    version="0.1.0",
     author="Li Xing",
     author_email="lix930701@gmail.com",
     description="implement d-tailor method using dnachisel specs",
